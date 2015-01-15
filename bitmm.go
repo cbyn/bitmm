66d1a7d169d063f61f447db04181bed79ab122eb
Chris Bynum <cbynum@cdbcap.com>
2015-01-14 17:12:12 -0700

    basic concurrent api communication

--- bitmm.go
+++ bitmm.go
@@ -15,54 +15,54 @@ var (
 )
 
 func main() {
+	bookChan := make(chan bitfinex.Book)
+	tradesChan := make(chan bitfinex.Trades)
+
 	for {
 		start := time.Now()
-		book, bTime, bErr := processBook()
-		trades, tTime, tErr := processTrades()
-		clearScreen()
-		printResults(book, bTime, bErr, trades, tTime, tErr)
-		fmt.Printf("%v total\n", time.Since(start))
+
+		// get data in separate goroutines
+		go processBook(bookChan)
+		go processTrades(tradesChan)
+
+		// block until both are done
+		printResults(<-bookChan, <-tradesChan)
+
+		fmt.Printf("\n%v to get data\n", time.Since(start))
 	}
 }
 
-func processBook() (bitfinex.Book, time.Duration, error) {
-	start := time.Now()
-	trades, err := apiPublic.Orderbook("ltcusd", 5, 5)
-	return trades, time.Since(start), err
+// Get book data and send to channel
+func processBook(bookChan chan bitfinex.Book) {
+	book, _ := apiPublic.Orderbook("btcusd", 5, 5)
+	bookChan <- book
 }
 
-func processTrades() (bitfinex.Trades, time.Duration, error) {
-	start := time.Now()
-	trades, err := apiPublic.Trades("ltcusd", 5)
-	return trades, time.Since(start), err
+// Get trade data and send to channel
+func processTrades(tradesChan chan bitfinex.Trades) {
+	trades, _ := apiPublic.Trades("btcusd", 5)
+	tradesChan <- trades
 }
 
 // Print results
-func printResults(book bitfinex.Book, bTime time.Duration, bErr error, trades bitfinex.Trades, tTime time.Duration, tErr error) {
-	if bErr != nil {
-		fmt.Println(bErr)
-	} else {
-		fmt.Println("----------------------------")
-		fmt.Printf("%-10s%-10s%8s\n", " Bid", "  Ask", "Size ")
-		fmt.Println("----------------------------")
-		for i := range book.Asks {
-			item := book.Asks[len(book.Asks)-1-i]
-			fmt.Printf("%-10s%-10.4f%8.2f\n", "", item.Price, item.Amount)
-		}
-		for _, item := range book.Bids {
-			fmt.Printf("%-10.4f%-10.2s%8.2f\n", item.Price, "", item.Amount)
-		}
-		fmt.Println("----------------------------")
+func printResults(book bitfinex.Book, trades bitfinex.Trades) {
+	clearScreen()
+
+	fmt.Println("----------------------------")
+	fmt.Printf("%-10s%-10s%8s\n", " Bid", "  Ask", "Size ")
+	fmt.Println("----------------------------")
+	for i := range book.Asks {
+		item := book.Asks[len(book.Asks)-1-i]
+		fmt.Printf("%-10s%-10.4f%8.2f\n", "", item.Price, item.Amount)
 	}
-	if tErr != nil {
-		fmt.Println(tErr)
-	} else {
-		fmt.Println("\nLast Trades:")
-		for _, trade := range trades {
-			fmt.Printf("%-6.4f - size: %6.2f\n", trade.Price, trade.Amount)
-		}
-		fmt.Printf("\n%v to get book data\n", bTime)
-		fmt.Printf("%v to get trade data\n", tTime)
+	for _, item := range book.Bids {
+		fmt.Printf("%-10.4f%-10.2s%8.2f\n", item.Price, "", item.Amount)
+	}
+	fmt.Println("----------------------------")
+
+	fmt.Println("\nLast Trades:")
+	for _, trade := range trades {
+		fmt.Printf("%-6.4f - size: %6.2f\n", trade.Price, trade.Amount)
 	}
 }
 
