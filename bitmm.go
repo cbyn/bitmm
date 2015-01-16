239086d715cca890a9f84efbdb134e88b88b382d
Chris Bynum <cbynum@cdbcap.com>
2015-01-15 21:56:53 -0700

    initial implementation of basic market making

--- bitmm.go
+++ bitmm.go
@@ -1,13 +1,26 @@
-// This only shows a streaming orderbook right now
+// TODO: quit on terminal input
+// TODO: make price a volume wieghted moving average
 
 package main
 
 import (
 	"./bitfinex"
 	"fmt"
+	"log"
+	"math"
 	"os"
 	"os/exec"
 	"time"
+)
+
+// Trade inputs
+const (
+	SYMBOL    = "ltcusd" // Instrument to trade
+	MINCHANGE = 0.0005   // Minumum change required to update prices
+	TRADENUM  = 10       // Number of trades to use in calculations
+	AMOUNT    = 0.10     // Size to trade
+	BIDEDGE   = 0.01     // Required edge for a buy order
+	ASKEDGE   = 0.01     // Required edge for a sell order
 )
 
 var (
@@ -15,37 +28,130 @@ var (
 )
 
 func main() {
+	fmt.Println("\nStarting up...")
+
+	// Create channels for exchange communication
 	bookChan := make(chan bitfinex.Book)
 	tradesChan := make(chan bitfinex.Trades)
+	bidChan := make(chan bitfinex.Order)
+	askChan := make(chan bitfinex.Order)
+
+	// Initial orders
+	bid, ask := createOrders()
+
+	var (
+		trades bitfinex.Trades
+		book   bitfinex.Book
+		start  time.Time
+		theo   float64
+	)
 
 	for {
-		start := time.Now()
+		start = time.Now()
 
-		// get data in separate goroutines
+		// Get data in separate goroutines
 		go processBook(bookChan)
 		go processTrades(tradesChan)
 
-		// block until both are done
-		printResults(<-bookChan, <-tradesChan)
+		// Modify orders in separate goroutines when trade data returns
+		trades = <-tradesChan
+		theo = calculateTheo(trades)
+		go replaceBid(bid, bidChan, theo)
+		go replaceAsk(ask, askChan, theo)
 
-		fmt.Printf("\n%v to get data\n", time.Since(start))
+		// Print data and current orders when all communication is finished
+		bid = <-bidChan
+		ask = <-askChan
+		book = <-bookChan
+		printResults(book, trades, bid, ask)
+
+		// Print processing time
+		fmt.Printf("\n%v processing time...", time.Since(start))
 	}
+}
+
+// Create initial orders
+func createOrders() (bitfinex.Order, bitfinex.Order) {
+	// Get the current price
+	trades, err := api.Trades(SYMBOL, 1)
+	checkErr(err)
+	price := trades[0].Price
+
+	// Order parameters
+	params := []bitfinex.OrderParams{
+		{SYMBOL, AMOUNT, price - BIDEDGE, "bitfinex", "buy", "limit"},
+		{SYMBOL, AMOUNT, price + ASKEDGE, "bitfinex", "sell", "limit"},
+	}
+
+	// Send new order request to the exchange
+	orders, err := api.MultipleNewOrders(params)
+	checkErr(err)
+
+	return orders.Orders[0], orders.Orders[1]
 }
 
 // Get book data and send to channel
 func processBook(bookChan chan<- bitfinex.Book) {
-	book, _ := api.Orderbook("btcusd", 5, 5)
+	book, err := api.Orderbook(SYMBOL, 5, 5)
+	checkErr(err)
+
 	bookChan <- book
 }
 
 // Get trade data and send to channel
 func processTrades(tradesChan chan<- bitfinex.Trades) {
-	trades, _ := api.Trades("btcusd", 10)
+	trades, err := api.Trades(SYMBOL, TRADENUM)
+	checkErr(err)
+
 	tradesChan <- trades
 }
 
+// Calculate a volume-weighted moving average of trades
+func calculateTheo(trades bitfinex.Trades) float64 {
+	var sum1, sum2 float64
+	for _, trade := range trades {
+		sum1 += trade.Price * trade.Amount
+		sum2 += trade.Amount
+	}
+	return sum1 / sum2
+}
+
+// Modify bid order and send to channel
+func replaceBid(bid bitfinex.Order, bidChan chan<- bitfinex.Order, theo float64) {
+	price := theo - BIDEDGE
+
+	var err error
+	if math.Abs(price-bid.Price) >= MINCHANGE {
+		bid, err = api.ReplaceOrder(bid.ID, SYMBOL, AMOUNT, price, "bitfinex", "buy", "limit")
+		checkErr(err)
+		if bid.ID == 0 {
+			cancelAll()
+			log.Fatal("Failed to replace bid order")
+		}
+	}
+
+	bidChan <- bid
+}
+
+// Modify ask order and send to channel
+func replaceAsk(ask bitfinex.Order, askChan chan<- bitfinex.Order, theo float64) {
+	price := theo + ASKEDGE
+
+	var err error
+	if math.Abs(price-ask.Price) >= MINCHANGE {
+		ask, err = api.ReplaceOrder(ask.ID, SYMBOL, AMOUNT, price, "bitfinex", "sell", "limit")
+		checkErr(err)
+		if ask.ID == 0 {
+			cancelAll()
+			log.Fatal("Failed to replace ask order")
+		}
+	}
+
+	askChan <- ask
+}
+
 // Print results
-func printResults(book bitfinex.Book, trades bitfinex.Trades) {
+func printResults(book bitfinex.Book, trades bitfinex.Trades, bid, ask bitfinex.Order) {
 	clearScreen()
 
 	fmt.Println("----------------------------")
@@ -64,6 +170,17 @@ func printResults(book bitfinex.Book, trades bitfinex.Trades) {
 	for _, trade := range trades {
 		fmt.Printf("%-6.4f - size: %6.2f\n", trade.Price, trade.Amount)
 	}
+
+	fmt.Printf("\nCurrent Bid: %6.4f\n", bid.Price)
+	fmt.Printf("Current Ask: %6.4f\n", ask.Price)
+}
+
+// Exit on any errors
+func checkErr(err error) {
+	if err != nil {
+		cancelAll()
+		log.Fatal(err)
+	}
 }
 
 // Clear the terminal between prints
@@ -71,4 +188,13 @@ func clearScreen() {
 	c := exec.Command("clear")
 	c.Stdout = os.Stdout
 	c.Run()
+}
+
+// Cancel all orders
+func cancelAll() {
+	cancelled := false
+	for !cancelled {
+		cancelled, _ = api.CancelAll()
+	}
+	fmt.Println("\nALL ORDERS HAVE BEEN CANCELLED")
 }
