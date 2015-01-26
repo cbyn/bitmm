3c703d2f0ebde223ccf333b0ed6921135addea4f
Chris Bynum <cbynum@cdbcap.com>
2015-01-26 15:21:58 -0700

    testing for number of orders created

---
+++ bitmm_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bitmm/bitfinex"
+	"github.com/davecgh/go-spew/spew"
+	"testing"
+)
+
+func TestCalcOrderParams(t *testing.T) {
+	MAXO = 100
+	INEDGE = 0.05
+	OUTEDGE = 0.01
+	var params []bitfinex.OrderParams
+	// Test long postions
+	params = calcOrderParams(100-MINO/2, 2.00)
+	if len(params) != 1 {
+		t.Fatal("Should only create one order")
+	}
+	params = calcOrderParams(100-MINO*2, 2.00)
+	if len(params) != 3 {
+		t.Fatal("Should create three orders")
+	}
+	params = calcOrderParams(100*2, 2.00)
+	if len(params) != 1 {
+		t.Fatal("Should only create one order")
+	}
+	params = calcOrderParams(MINO/2, 2.00)
+	if len(params) != 2 {
+		t.Fatal("Should create two orders")
+	}
+	// Test short positions
+	params = calcOrderParams(-100+MINO/2, 2.00)
+	if len(params) != 1 {
+		t.Fatal("Should only create one order")
+	}
+	params = calcOrderParams(-100+MINO*2, 2.00)
+	if len(params) != 3 {
+		t.Fatal("Should create three orders")
+	}
+	params = calcOrderParams(-100*2, 2.00)
+	if len(params) != 1 {
+		t.Fatal("Should only create one order")
+	}
+	params = calcOrderParams(-MINO/2, 2.00)
+	if len(params) != 2 {
+		t.Fatal("Should create two orders")
+	}
+}
