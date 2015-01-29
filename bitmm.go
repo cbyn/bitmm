78ed7f4347bcbf05d37dc2d9620502485ffe6653
Chris Bynum <cbynum@cdbcap.com>
2015-01-29 13:08:05 -0700

    redirect log to file

--- bitmm.go
+++ bitmm.go
@@ -43,6 +43,14 @@ func main() {
 
 	fmt.Println("\nInitializing...")
 
+	// Set file for logging
+	logFile, err := os.OpenFile("bitmm_log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
 	// Get maxPos, minEdge, exitPercent from user input
 	getArgs()
 
