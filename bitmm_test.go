35935d4f54a2e79563eab94d69a651e08a9d090d
Chris Bynum <cbynum@cdbcap.com>
2015-02-04 14:43:17 -0700

    fix broken test

--- bitmm_test.go
+++ bitmm_test.go
@@ -2,53 +2,57 @@ package main
 
 import (
 	"bitmm/bitfinex"
+	"code.google.com/p/gcfg"
 	// "github.com/davecgh/go-spew/spew"
 	"testing"
 )
 
 func TestCalcOrderParams(t *testing.T) {
-	maxPos = 100
-	minEdge = 0.02
-	exitPercent = 0.25
+	// Get config info
+	err := gcfg.ReadFileInto(&cfg, "bitmm.gcfg")
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	var params []bitfinex.OrderParams
 	// Test long postions
-	params = calculateOrderParams(100-MINO/2, 2.00, 0.04)
+	params = calculateOrderParams(cfg.Sec.MaxPos-cfg.Sec.MinPos/2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 1 {
 		t.Fatal("Should only create one order")
 	}
-	params = calculateOrderParams(100-MINO*2, 2.00, 0.01)
+	params = calculateOrderParams(cfg.Sec.MaxPos-cfg.Sec.MinPos*2, 2.00, 0.01)
 	// spew.Dump(params)
 	if len(params) != 3 {
 		t.Fatal("Should create three orders")
 	}
-	params = calculateOrderParams(100*2, 2.00, 0.04)
+	params = calculateOrderParams(cfg.Sec.MaxPos*2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 1 {
 		t.Fatal("Should only create one order")
 	}
-	params = calculateOrderParams(MINO/2, 2.00, 0.04)
+	params = calculateOrderParams(cfg.Sec.MinPos/2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 2 {
 		t.Fatal("Should create two orders")
 	}
 	// Test short positions
-	params = calculateOrderParams(-100+MINO/2, 2.00, 0.04)
+	params = calculateOrderParams(-cfg.Sec.MaxPos+cfg.Sec.MinPos/2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 1 {
 		t.Fatal("Should only create one order")
 	}
-	params = calculateOrderParams(-100+MINO*2, 2.00, 0.04)
+	params = calculateOrderParams(-cfg.Sec.MaxPos+cfg.Sec.MinPos*2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 3 {
 		t.Fatal("Should create three orders")
 	}
-	params = calculateOrderParams(-100*2, 2.00, 0.04)
+	params = calculateOrderParams(-cfg.Sec.MaxPos*2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 1 {
 		t.Fatal("Should only create one order")
 	}
-	params = calculateOrderParams(-MINO/2, 2.00, 0.04)
+	params = calculateOrderParams(-cfg.Sec.MinPos/2, 2.00, 0.04)
 	// spew.Dump(params)
 	if len(params) != 2 {
 		t.Fatal("Should create two orders")
